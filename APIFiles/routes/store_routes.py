6f6e3add6a0cd3d0d3a7ec34bb69fa3975c80fa8
brendansailer <57545584+brendansailer@users.noreply.github.com>
2021-04-05 19:20:57 -0400

    Add store deletion route

--- APIFiles/routes/store_routes.py
+++ APIFiles/routes/store_routes.py
@@ -1,6 +1,8 @@
 from flask import request, jsonify, Blueprint
 from models.base_model import db
 from models.store_model import Store
+from models.listItem_model import ListItem
+from models.listOwnership_model import ListOwnership
 from schemas.store_schema import store_schema
 from sqlalchemy import exc
 
@@ -14,6 +16,32 @@ def add_store():
   new_store = Store(store_name)
 
   db.session.add(new_store)
+  try:
+    db.session.commit()
+  except exc.SQLAlchemyError:
+    print(exc.SQLAlchemyError)
+    return {"result": False}
+
+  return {"result": True}
+
+# Delete a store (and all of its items!)
+@store_api.route('/store/delete', methods=['POST'])
+def delete_store():
+  store_id = request.json['store_id'] # store ids are unique to each list but for safety we require the list_id anyways
+  list_id = request.json['list_id']
+  uuid = request.json["uuid"]
+
+  owner = db.session.query(ListOwnership).filter(ListOwnership.uuid==uuid).filter(ListOwnership.list_id==list_id).all()
+  
+  if not owner: # Make sure they have permission to delete this store
+    print("INVALID REQUEST - No permissions")
+    return {"result": False}
+
+  items = ListItem.query.filter(ListItem.store_id==store_id).all() # Get all items for the list with list_id == list_id
+
+  for item in items:
+    db.session.delete(item)
+
   try:
     db.session.commit()
   except exc.SQLAlchemyError:
