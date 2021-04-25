1a671e728a3038c130d3c1a0406c3fcd5b9ba805
Brennen <bhogan1@nd.edu>
2021-04-24 21:49:05 -0400

    Corner Radius on Animation (For Jake)

--- AppFiles/GroceryGuru/GroceryGuru/LaunchAnimationViewController.swift
+++ AppFiles/GroceryGuru/GroceryGuru/LaunchAnimationViewController.swift
@@ -11,7 +11,8 @@ class LaunchAnimationViewController: UIViewController {
     
     private let imageView: UIImageView = {
         let imageView = UIImageView(frame: CGRect(x: 67, y: 293, width: 256, height: 256))
-        imageView.image = UIImage(named:"GG_Logo_256")
+        imageView.layer.cornerRadius = 10
+        imageView.image = UIImage(named:"GG_Logo_Clear_256")
         return imageView
     }()
     
@@ -28,16 +29,17 @@ class LaunchAnimationViewController: UIViewController {
     }
     
     private func animate(){
-        UIView.animate(withDuration: 0.75, animations: {
+        UIView.animate(withDuration: 0.8, animations: {
             self.imageView.frame = CGRect(
                 x: 131,
                 y: 101,
                 width: 128,
                 height: 128
             )
+            self.imageView.backgroundColor = UIColor(hex: 0x617757)
             self.view.backgroundColor = UIColor.white
         }){ (done) in
-            DispatchQueue.main.asyncAfter(deadline: .now()+0.5, execute: {
+            DispatchQueue.main.asyncAfter(deadline: .now()+0.55, execute: {
                 let sb = UIStoryboard(name: "Main", bundle: nil)
                 let vc = sb.instantiateInitialViewController()
                 UIApplication.shared.keyWindow?.rootViewController = vc
