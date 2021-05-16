18ab349fc3dd21f91e609eda0963b6c1b87170e2
Brennen <bhogan1@nd.edu>
2021-05-16 19:10:47 -0400

    Polish: Auto scrolling to prevent keyboard blocking new field

--- AppFiles/GroceryGuru/GroceryGuru/ViewControllers/DetailedRecipe/DetailedRecipeController.swift
+++ AppFiles/GroceryGuru/GroceryGuru/ViewControllers/DetailedRecipe/DetailedRecipeController.swift
@@ -43,6 +43,20 @@ class DetailedRecipeController: UIViewController {
         tableView.rowHeight = 30
         
         self.getData()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+
+    }
+    
+    @objc private func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+        }
+    }
+    
+    @objc private func keyboardWillHide(notification: NSNotification) {
+        tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
     
     override func viewDidAppear(_ animated: Bool) {
--- AppFiles/GroceryGuru/GroceryGuru/ViewControllers/RecipieLanding/RecipePageViewController.swift
+++ AppFiles/GroceryGuru/GroceryGuru/ViewControllers/RecipieLanding/RecipePageViewController.swift
@@ -35,6 +35,21 @@ class RecipePageViewController: UIViewController {
         tableView.dataSource = self
                 
         self.getData()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+
+
+    }
+    
+    @objc private func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+        }
+    }
+    
+    @objc private func keyboardWillHide(notification: NSNotification) {
+        tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
     
     override func viewDidAppear(_ animated: Bool) {
